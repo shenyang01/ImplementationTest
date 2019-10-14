5f4c9f0cd8d4eae1f9ea57609e4cf538d888b53a
shenyang <437798373@qq.com>
2019-10-14 17:43:16 +0800

    修改

--- app/src/main/java/com/sy/implementationtest/MainActivity.java
+++ app/src/main/java/com/sy/implementationtest/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.sy.TestUtils;
+import com.sy.testlibray2.libtray2;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
