81332503fc1ee2dfb75663fcb4cfb17d89c2ba24
shenyang <437798373@qq.com>
2019-10-14 18:02:29 +0800

    修改

--- app/src/main/java/com/sy/implementationtest/MainActivity.java
+++ app/src/main/java/com/sy/implementationtest/MainActivity.java
@@ -5,12 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.sy.TestUtils;
-import com.sy.testlibray2.libtray2;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ mylibrary2/src/main/java/com/sy/mylibrary2/Testhaha.java
@@ -0,0 +1,13 @@
+package com.sy.mylibrary2;
+
+import android.util.Log;
+
+/**
+ * @ data  2019/10/14 18:00
+ * @ author  zxcg
+ */
+public class Testhaha {
+    public void print(){
+        Log.e("tag","Testhaha");
+    }
+}
